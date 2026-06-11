72ad3490757925e7a6d046ff4d26773c6fe3c653
Guido Günther <agx@sigxcpu.org>
2026-06-11 14:13:14 +0200

    panels/user: Allow to add auxiliary groups
    We parse a file with config defaults. This will be useful
    to configure other panels too.
    
    Closes: https://gitlab.gnome.org/World/Phosh/phosh-first-boot/-/work_items/5
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-first-boot/-/merge_requests/8>

--- src/pfb/config.rs
+++ src/pfb/config.rs
@@ -1,4 +1,5 @@
 pub static GETTEXT_PACKAGE: &str = "PhoshFirstBoot";
 pub static APP_ID: &str = "mobi.phosh.FirstBoot";
+pub static DEFAULTSDIR: &str = "/usr/local/share/phosh-first-boot";
 pub static LOCALEDIR: &str = "/usr/local/share/locale";
 pub static OUTPUTDIR: &str = "/run/phosh-first-boot";
---
+++ src/pfb/defaults.rs
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: 2026 Phosh.mobi e.V.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use log::warn;
+use serde::Deserialize;
+use std::path::Path;
+
+#[derive(Debug, Deserialize, Default)]
+pub struct Defaults {
+    pub user: UserDefaults,
+}
+
+#[derive(Debug, Deserialize, Default)]
+pub struct UserDefaults {
+    // Auxiliary groups we add the user to
+    pub aux_groups: Vec<String>,
+}
+
+impl Defaults {
+    pub fn load<P: AsRef<Path>>(path: P) -> Self {
+        let contents = match std::fs::read_to_string(&path) {
+            Ok(c) => c,
+            Err(err) => {
+                warn!(
+                    "Failed to load default config '{}': {err}",
+                    path.as_ref().display()
+                );
+                return Self::default();
+            }
+        };
+
+        toml::from_str(&contents).unwrap_or_else(|err| {
+            warn!(
+                "Failed to parse default config '{}': {err}",
+                path.as_ref().display()
+            );
+            Self::default()
+        })
+    }
+}
