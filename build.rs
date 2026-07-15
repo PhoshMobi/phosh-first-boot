7be25b6f672b7dab1973e2c8a76d9a85f55f6368
Guido Günther <agx@sigxcpu.org>
2026-07-15 18:54:28 +0200

    build: Skip meson run during Debian builds
    We run meson to set the right prefix, running it again will
    overwrite this.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-first-boot/-/merge_requests/8>

--- build.rs
+++ build.rs
@@ -8,24 +8,26 @@ fn main() {
         "mobi.phosh.FirstBoot.gresource",
     );
 
-    let meson_dir = format!("{}/_build/", env::var("OUT_DIR").unwrap());
-    Command::new("meson")
-        .args(["setup", "--reconfigure", &meson_dir])
-        .status()
-        .unwrap();
-    Command::new("meson")
-        .args(["compile", "-C", &meson_dir])
-        .status()
-        .unwrap();
+    if env::var("DEB_HOST_ARCH").is_err() {
+        let meson_dir = format!("{}/_build/", env::var("OUT_DIR").unwrap());
+        Command::new("meson")
+            .args(["setup", "--reconfigure", &meson_dir])
+            .status()
+            .unwrap();
+        Command::new("meson")
+            .args(["compile", "-C", &meson_dir])
+            .status()
+            .unwrap();
 
-    let src = format!("{}/data/gschemas.compiled", &meson_dir);
-    // Would be nicer to have it in target/<profile> but .cargo/toml can't have variables
-    // in env vars
-    let dst = format!(
-        "{}/target/gschemas.compiled",
-        env::var("CARGO_MANIFEST_DIR").unwrap()
-    );
-    std::fs::copy(src, dst).unwrap();
+        let src = format!("{}/data/gschemas.compiled", &meson_dir);
+        // Would be nicer to have it in target/<profile> but .cargo/toml can't have variables
+        // in env vars
+        let dst = format!(
+            "{}/target/gschemas.compiled",
+            env::var("CARGO_MANIFEST_DIR").unwrap()
+        );
+        std::fs::copy(src, dst).unwrap();
+    }
 
     println!("cargo::rerun-if-changed=meson.build");
     println!("cargo::rerun-if-changed=src/meson.build");
